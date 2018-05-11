b47762a4f5913aa219f21477498ccbff6ac7ed6b
Bilel <Bilel@LAPTOP-817TIV08>
2018-05-11 15:42:03 +0200

    facciamo una seconda prova

--- schoolManager/src/schoolManager/Hour.java
+++ schoolManager/src/schoolManager/Hour.java
@@ -15,12 +15,32 @@ import java.util.*;
 
 public class Hour {
     private int idHour;
-    public Hour(int id) {
+    private int begin;
+    private int end;
+    public Hour(int id,int begin,int end) {
        this.idHour=id;
+       this.begin=begin;
+       this.end=end;
+    }
+
+    public int getBegin() {
+        return begin;
+    }
+
+    public int getEnd() {
+        return end;
     }
 
     public int getIdHour() {
         return idHour;
+    }
+
+    public void setBegin(int begin) {
+        this.begin = begin;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
     }
 
     public void setIdHour(int idHour) {
